ce8a0b9347de23c15190c557d7326819936804df
nkabylbay <nkabylbai@gmail.com>
2020-08-03 01:37:46 +0600

    Initial commit

---
+++ tbackend/src/main/java/kz/terengeboilai/web/TerengbApplication.java
@@ -0,0 +1,13 @@
+package kz.terengeboilai.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerengbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TerengbApplication.class, args);
+  }
+
+}
---
+++ tbackend/src/test/java/kz/terengeboilai/web/TerengbApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.terengeboilai.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerengbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
